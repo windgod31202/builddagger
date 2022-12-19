834038d6367cde5dd467291c544c07eb0bbcf5d8
Jaychen <jay0345678@gmail.com>
2022-12-19 17:21:30 +0800

    create worker Singleton

---
+++ app/src/main/java/com/example/building_dagger/worker/Worker.java
@@ -0,0 +1,13 @@
+package com.example.building_dagger.worker;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class Worker {
+
+    @Inject
+    public Worker(){
+
+    }
+}
